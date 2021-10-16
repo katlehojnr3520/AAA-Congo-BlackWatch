5062e681f1dfc6933a5e44475b2626eba082203f
katlehojnr3520 <katlehojnr3520@gmail.com>
2021-10-16 20:20:06 +0200

    12

---
+++ cmake-build-debug/CMakeFiles/AAA_Congo_BlackWatch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AAA_Congo_BlackWatch.exe"
+  "AAA_Congo_BlackWatch.exe.manifest"
+  "AAA_Congo_BlackWatch.pdb"
+  "CMakeFiles/AAA_Congo_BlackWatch.dir/Board.cpp.obj"
+  "libAAA_Congo_BlackWatch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AAA_Congo_BlackWatch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
